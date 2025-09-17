410c7c5d8d5fcad90e8c9ae4224338359e8c718b
Luch4os <melonecta@gmail.com>
2025-09-17 13:58:53 -0300

    Create teste.py
    testando python

---
+++ teste.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+
+# vou fazer uma automação para entrar na shoppee e pesquisas um produto
+
+# abrir o navegador
+
+pyautogui.pause = 0.5
+
+pyautogui.press("win")
+
+
+pyautogui.write("Chrome")
+
+pyautogui.press("enter")
+
+time.sleep(2)
+
+# Acessar site
+
+pyautogui.write("https://shopee.com.br")
+
+pyautogui.press("enter")
+
+# como aumentar tela???
